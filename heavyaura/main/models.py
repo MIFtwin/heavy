d530c4edd0efb0a6b55b37684493b85dd0748d6b
Mif <the.mullayarov.twins@gmail.com>
2025-04-03 10:04:59 +0500

    Добавлена структура проекта и статические ресурсы

---
+++ heavyaura/main/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Category:
+    name = models.CharField(max_length=20,
+                            unique=True)
+    slug = models.SlugField(max_length=20,
+                            unique=True)
+    
+
+    class Meta:
+        ordering = ['name']
+        indexes = [models.Index(fields=['name'])]
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+
+    def __str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    category = models.ForeignKey(Category,
+                                 related_name='products',
+                                 on_delete=models.CASCADE)
